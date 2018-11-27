5a26b2756e0708865310bf68cd48124fc4ae2149
marcin holota <marcinholota1@gmail.com>
2018-11-27 19:38:38 +0100

    initial commit

---
+++ src/main/java/com/justLearnIT/JustLearnItApplication.java
@@ -0,0 +1,12 @@
+package com.justLearnIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustLearnItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustLearnItApplication.class, args);
+	}
+}
