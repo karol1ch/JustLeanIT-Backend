a69a6d1a6c05d62b3bccef4220aed6b9a5aabbdf
marcin holota <marcinholota1@gmail.com>
2018-12-20 00:20:32 +0100

    constructors fixed

--- src/main/java/com/justLearnIT/model/Test.java
+++ src/main/java/com/justLearnIT/model/Test.java
@@ -1,11 +1,14 @@
 package com.justLearnIT.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Data
 @Builder
 @Entity
